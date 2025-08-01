2f0c02e25376e53d13d836f26f1b375310de436f
Divijv8 <divijvermav8@gmail.com>
2025-08-01 19:11:30 +0530

    edit one

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+const { chang } = require("wagmi/chains");
+
 let activeTabId = null;
 let activeDomain = null;
 let startTime = null;
@@ -33,3 +35,38 @@ async function stopTimer(){
     startTime = null;
 }
 
+function startTimer(domain){
+    if(domain){
+        activeDomain = domain;
+        startTime = new Date();
+    }
+}
+
+// event listener for tab updates
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+    await stopTimer();
+    activeTabId = activeInfo.tabId;
+    try{
+        const tab = await chrome.tabs.get(activeTabId);
+        if(tab && tab.url){
+            const domain = getDomain(tab.url);
+            if(domain){
+                startTimer(domain);
+            }
+        }
+    } catch(e){
+        console.error('Error fetching tab info:', e);
+    }
+});
+
+// On the tab update: URL int the active tab changes
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if(tabId === activeTabId && changeInfo.url){
+        await stopTimer();
+        const domain = getDomain(changeInfo.url);
+        startTimer(domain);
+    }
+});
+
+chrome.windows.onFocusChanged.addLi
